95f0c0db4554f6370f38f66b9d8aa8971866caa8
Vejron <bjorn.yttergren@gmail.com>
2017-05-12 15:17:14 +0200

    started on map-view component

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Output, Input, EventEmitter, ElementRef } from '@angular/core';
+
+declare var google: any;
 
 @Component({
-  selector: 'app-map-view',
-  templateUrl: './map-view.component.html',
-  styleUrls: ['./map-view.component.scss']
+   selector: 'app-map-view',
+   templateUrl: './map-view.component.html',
+   styleUrls: ['./map-view.component.scss']
 })
 export class MapViewComponent implements OnInit {
 
-  constructor() { }
+   @Input() options: any;
 
-  ngOnInit() {
-  }
+   @Output() onMapReady: EventEmitter<any> = new EventEmitter();
+
+   public map: any;
+
+   constructor(public element: ElementRef) { }
+
+
+
+   ngOnInit() {
+   }
+
+   ngAfterViewInit() {
+      this.map = new google.maps.Map(this.element.nativeElement.children[0], this.options);
+      this.onMapReady.emit({
+         map: this.map
+      });
+
+      if (this.overlays) {
+         for (let overlay of this.overlays) {
+            overlay.setMap(this.map);
+            this.bindOverlayEvents(overlay);
+         }
+      }
+
+      this.map.addListener('click', (event) => {
+         this.zone.run(() => {
+            this.onMapClick.emit(event);
+         });
+      });
+   }
+
+   getMap() {
+      return this.map;
+   }
 
 }
