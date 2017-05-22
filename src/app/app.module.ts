894df6be7692121b73b00da5c3098cfdee7b4af1
Vejron <bjorn.yttergren@gmail.com>
2017-05-22 14:55:18 +0200

    dialog add marker simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdButtonToggleModule, MdCheckboxModule, MdIconModule, MdDialogModule, } from '@angular/material';
+import { MdButtonModule, MdButtonToggleModule, MdCheckboxModule, MdIconModule, MdDialogModule } from '@angular/material';
 
 import { AreaService } from './shared/services/area.service'
 import { LiveTrackingService } from './shared/services/live-tracking.service'
@@ -41,7 +41,7 @@ import { NotificationLayerComponent, DialogOverviewExampleDialog} from './map-co
       FormsModule,
       HttpModule,
       BrowserAnimationsModule,
-      MdButtonModule, MdCheckboxModule, MdButtonToggleModule, MdIconModule, MdDialogModule
+      MdButtonModule, MdCheckboxModule, MdButtonToggleModule, MdIconModule, MdDialogModule, 
    ],
    providers: [
       AreaService,
--- src/app/map-container/notification-layer/notification-layer.component.ts
+++ src/app/map-container/notification-layer/notification-layer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MdDialog } from '@angular/material';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
 import { } from '@types/googlemaps';
 
 @Component({
@@ -13,7 +13,7 @@ export class NotificationLayerComponent implements OnInit {
    isActive = false;
    private clickEvent: google.maps.MapsEventListener;
 
-   constructor(public dialog: MdDialog) { }
+   constructor(public dialog: MdDialog, public zone: NgZone) { }
 
    ngOnInit() {
    }
@@ -23,27 +23,59 @@ export class NotificationLayerComponent implements OnInit {
          this.isActive = true;
          this.map.set('draggableCursor', 'crosshair');
          this.clickEvent = this.map.addListener('click', (e) => {
-            console.log(e);
-            this.openDialog();
+            // invoke via zone as this event is something angular is not aware of
+            this.zone.run(() => this.openDialog(e.latLng))
          })
       } else {
          this.isActive = false;
-         this.map.set('draggableCursor', 'default');
+         this.map.set('draggableCursor', 'move');
          google.maps.event.removeListener(this.clickEvent);
       }
 
    }
 
-   openDialog() {
-      this.dialog.open(DialogOverviewExampleDialog);
-   }
+   openDialog(latLng: google.maps.LatLng) {
+      let dialogRef = this.dialog.open(DialogOverviewExampleDialog);
 
+      dialogRef.afterClosed().subscribe(result => {
+         console.log('dialog closed');
+
+         if (result) {
+            // add marker to map
+            let marker = new google.maps.Marker({
+               position: latLng,
+               map: this.map,
+               title: result
+            });
+         }
+      })
+   }
 }
 
 @Component({
-   selector: 'dialog-overview-example-dialog',
-   template: `<p>
-            Multi-line inline template using back-ticks
-        </p>`,
+   template: `
+      <h2>Mark something</h2>
+
+      <md-button-toggle-group #group="mdButtonToggleGroup">
+         <md-button-toggle value="info">
+            <md-icon>info_outline</md-icon>
+         </md-button-toggle>
+         <md-button-toggle value="warning">
+            <md-icon [color]="red">warning</md-icon>
+         </md-button-toggle>
+         <md-button-toggle value="gas">
+            <md-icon>local_gas_station</md-icon>
+         </md-button-toggle>
+         <md-button-toggle value="notification">
+            <md-icon>notifications</md-icon>
+         </md-button-toggle>
+      </md-button-toggle-group>
+      
+      <p class="example-selected-value">Selected value: {{group.value}}</p>
+
+      <button md-raised-button [disabled]="group.value === null" (click)="dialogRef.close(group.value)">Ok</button>
+      <button md-raised-button (click)="dialogRef.close()">Cancel</button>`
 })
-export class DialogOverviewExampleDialog { }
+export class DialogOverviewExampleDialog {
+   constructor(public dialogRef: MdDialogRef<any>) { }
+}
